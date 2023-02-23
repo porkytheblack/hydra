003ecc7b99c651160eed4751983319baa7e9ece8
Donald Onyino <58218526+porkytheblack@users.noreply.github.com>
2023-02-23 16:27:42 +0300

    fix(setup): changed loading of env variables

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"hydra/repository"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
@@ -21,9 +22,19 @@ func main() {
 }	
 
 func loadEnv() {
-	err := godotenv.Load(".env")
 
-	if err != nil {
-		fmt.Printf("An Error occured")
-	}
+	env := os.Getenv("GO_ENV")
+
+	if env == "" {
+		err := godotenv.Load(".env")
+
+		if err != nil {
+			fmt.Printf("An Error occured")
+			return
+		}
+		return
+	} 
+	
+	fmt.Printf("\n\nNo Env file current env is:: %s\n\n", env)
+	
 }
